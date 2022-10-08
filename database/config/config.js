dc9b5bad3579bbb59b454ed84faab7790b31dea3
jamilecadias <jamilecadias@gmail.com>
2022-10-08 20:55:25 -0300

    Creando un mode lo

--- database/config/config.js
+++ database/config/config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": 32496217,
+    "database": "conexao",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
---
+++ database/models/user.js
@@ -0,0 +1,39 @@
+/* const sequelize = require("sequelize"); */
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Users";
+    let cols ={
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true, 
+            autoIncrement: true
+        } ,
+        name: {
+            type: dataTypes.STRING 
+        }, 
+
+        tel: {
+            type: dataTypes.INTEGER
+        }, 
+        
+        email: {
+            type: dataTypes.STRING
+        },
+        
+        password: {
+            type: dataTypes.STRING
+        },
+        
+        avatar: {
+            type: dataTypes.STRING
+        }  
+
+    };
+    let config = {
+        tableName: "users",
+        timestamps: false
+    }
+    const User = sequelize.define(alias , cols , config)
+
+    return User;
+
+}
