b9b3eb6c77bd56865cc9d7474bc3d16101fc533b
jamilecadias <jamilecadias@gmail.com>
2022-10-09 00:23:39 -0300

    Creando rutas por POST

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,6 @@
+const db = require("../database/models")
+const Op = db.Sequelize.Op;
+
 const Controller = {
 
     index: (req, res) => {
@@ -12,13 +15,28 @@ const Controller = {
         res.render('register')
     }, 
 
+    create: (req, res)=>{
+        
+        res.send(req.body)
+    }, 
+
     dashboard: (req, res)=>{
         res.render('dashboard')
     }, 
 
     userUpdate: (req, res)=>{
         res.render('userUpdate')
-    }, 
+    },
+    
+    list: (req, res) => {
+        db.User.findAll()
+        .then(users => {
+              res.render("dashboard2",{users:users});
+           })
+           .catch(err => {
+              return res.send(err)
+           })
+     },
 
 
    /*  create: function(req, res) {
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -10,11 +10,17 @@ router.get('/', mainController.index);
 
 router.get('/register', mainController.register);
 
+router.post('/register', mainController.create);
+
 router.get('/login', mainController.login);
+
+router.post('/login', mainController.create);
 
 router.get('/dashboard', mainController.dashboard);
 
 router.get('/userUpdate', mainController.userUpdate);
+
+router.get('/users', mainController.list);
 
 
 /* router.get('/profile', mainController.profile); */
